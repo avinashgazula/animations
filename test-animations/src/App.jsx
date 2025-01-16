ba9e6a071d4c5685de1551119d97f6a7b6375332
avinashgazula <avinashgazula@gmail.com>
2025-01-16 07:13:04 +0530

    Add transform transition

--- test-animations/src/App.jsx
+++ test-animations/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
+import SimpleTransformTransition from './simple-transform-transition/SimpleTransformTransition';
 import StackedComponent from './stacked-component/StackedComponent';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +15,7 @@ function App() {
         
       </div>
       <StackedComponent />
+      <SimpleTransformTransition />
     </>
   )
 }
---
+++ test-animations/src/simple-transform-transition/SimpleTransformTransition.jsx
@@ -0,0 +1,12 @@
+import './SimpleTransformTransition.css';
+
+const SimpleTransformTransition = () => {
+    return (
+        <div className="box">
+            <div className="box-content">
+            </div>
+        </div>
+    )
+}   
+
+export default SimpleTransformTransition;
